9c7043bf363912877defa2f613f466fa9eac8fc4
TryKillMe-dev <65904061+TryKillMe-dev@users.noreply.github.com>
2020-05-27 16:07:01 +0200

    3

--- Projekt/Projekt/js/script.js
+++ Projekt/Projekt/js/script.js
@@ -1,12 +1,55 @@
-const mq = window.matchMedia( "(max-width: 400px)" );
+const mq = window.matchMedia("(max-width: 400px)");
 
 if (mq.matches) {
-$( 'li.submenu a[href="#"]').click(
-      function(e) {
-      	e.preventDefault();
-        $(this).next().toggle();
-        return false;
-    });
-}
+    $('li.submenu a[href="#"]').click(
+        function (e) {
+            e.preventDefault();
+            $(this).next().toggle();
+            return false;
+        });
 
 
+
+$('#big-slider').slick({
+    arrows: false,
+    infinite: true,
+    autoplay: true,
+  autoplaySpeed: 2000,
+    slidesToShow: 1,
+    adaptiveHeight: true
+
+});
+
+$('#small-slider').slick({
+  
+  slidesToShow: 2,
+  slidesToScroll: 1
+});
+
+
+$('#seo-slider').slick();
+    }
+
+
+$('#big-slider').slick({
+    arrows: false,
+    infinite: true,
+    autoplay: true,
+  autoplaySpeed: 2000,
+    slidesToShow: 1,
+    adaptiveHeight: true
+
+});
+
+$('#small-slider').slick({
+  infinite: true,
+  slidesToShow: 5,
+  slidesToScroll: 1
+});
+
+
+$('#seo-slider').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3
+});
