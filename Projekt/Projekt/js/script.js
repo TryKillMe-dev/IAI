8ed3abc8d3ace57f0682ab09512f5c7ef6e050fe
TryKillMe-dev <65904061+TryKillMe-dev@users.noreply.github.com>
2020-05-28 15:57:07 +0200

    day4

--- Projekt/Projekt/js/script.js
+++ Projekt/Projekt/js/script.js
@@ -7,49 +7,81 @@ if (mq.matches) {
             $(this).next().toggle();
             return false;
         });
+    
+    
+    $('li.subtopmenu a[href="#"]').click(
+        function (e) {
+            e.preventDefault();
+            $(this).next().toggle();
+            return false;
+        });
+    
+    
 
+
+    $('#big-slider').slick({
+        arrows: false,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        slidesToShow: 1,
+        adaptiveHeight: true
+
+    });
+
+    $('#small-slider').slick({
+
+        slidesToShow: 2,
+        slidesToScroll: 1
+    });
+
+
+    $('#seo-slider').slick();
+
+    $('.mid-slider').slick({
+        arrows: false,
+        centerMode: true,
+        centerPadding: '80px',
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        
+    });
+
+
+   
+    
+    
+
+}
 
 
 $('#big-slider').slick({
     arrows: false,
     infinite: true,
     autoplay: true,
-  autoplaySpeed: 2000,
+    autoplaySpeed: 2000,
     slidesToShow: 1,
     adaptiveHeight: true
 
 });
 
 $('#small-slider').slick({
-  
-  slidesToShow: 2,
-  slidesToScroll: 1
-});
-
-
-$('#seo-slider').slick();
-    }
-
-
-$('#big-slider').slick({
-    arrows: false,
     infinite: true,
-    autoplay: true,
-  autoplaySpeed: 2000,
-    slidesToShow: 1,
-    adaptiveHeight: true
-
-});
-
-$('#small-slider').slick({
-  infinite: true,
-  slidesToShow: 5,
-  slidesToScroll: 1
+    slidesToShow: 5,
+    slidesToScroll: 1
 });
 
 
 $('#seo-slider').slick({
-  infinite: true,
-  slidesToShow: 3,
-  slidesToScroll: 3
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3
 });
+
+$('.mid-slider').slick({
+    infinite: true,
+    arrows: false,
+    slidesToShow: 4,
+    slidesToScroll: 1
+});
+
