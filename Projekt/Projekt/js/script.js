4e523d2d9190465bddf97d717c6d2cbf4bf23409
TryKillMe-dev <65904061+TryKillMe-dev@users.noreply.github.com>
2020-06-15 10:12:04 +0200

    End

--- Projekt/Projekt/js/script.js
+++ Projekt/Projekt/js/script.js
@@ -7,16 +7,23 @@ if (mq.matches) {
             $(this).next().toggle();
             return false;
         });
+
+
+
     
-    
-    $('li.subtopmenu a[href="#"]').click(
-        function (e) {
-            e.preventDefault();
-            $(this).next().toggle();
-            return false;
-        });
-    
-    
+
+
+
+    function myFunction() {
+        var x = document.getElementById("tpmenu");
+        if (x.style.display === "block") {
+            x.style.display = "none";
+        } else {
+            x.style.display = "block";
+        }
+    }
+
+
 
 
     $('#big-slider').slick({
@@ -44,15 +51,97 @@ if (mq.matches) {
         centerPadding: '80px',
         slidesToShow: 1,
         slidesToScroll: 1,
-        
+
     });
 
 
-   
+    $('#slider_p').slick();
+
     
+    
+    
+    // Get the modal
+var modal = document.getElementById("myModal");
+var modal2 = document.getElementById("myModal2");
+var rozmiar = document.getElementById("rozmiar");
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+var span2 = document.getElementsByClassName("close2")[0];
+
+// When the user clicks the button, open the modal 
+
+    window.onload = function(){
+    btn.onclick = function (event) {
+    if (event.target == myBtn && rozmiar.value == "" || event.target == myBtn && rozmiar.value == "off") {
+
+
+        modal.style.display = "flex";
+    } else if (event.target == myBtn && rozmiar.value == "on") {
+        modal2.style.display = "flex";
+    }
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+   
+}
+span2.onclick = function () {
+   
+    modal2.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal || event.target == modal2 ) {
+        modal.style.display = "none";
+        modal2.style.display = "none";
+    }
+}
+    }
+    
+    
+    
+    $(document).ready(function(){
+    // HTML markup implementation, cover mode
+    $( '#menu' ).multilevelpushmenu({
+        containersToPush: [$( '#pushobj' )],
+        mode: 'cover',
+        collapsed: true,
+        
+        
+    });
+});
     
 
 }
+
+
+    
+
+
+
+$('.slider_prod').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider_nav'
+});
+
+$('.slider_nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.slider_prod',
+    arrows: true,
+    vertical: true,
+    focusOnSelect: true,
+    verticalSwiping: true,
+});
+
 
 
 $('#big-slider').slick({
@@ -81,7 +170,68 @@ $('#seo-slider').slick({
 $('.mid-slider').slick({
     infinite: true,
     arrows: false,
-    slidesToShow: 4,
-    slidesToScroll: 1
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 4,
+                slidesToScroll: 1
+            }
+
+    }]
+
 });
 
+
+
+$('li.subtopmenu a[href="#"]').click(
+    function (e) {
+        e.preventDefault();
+        $(this).next().toggle();
+        return false;
+    });
+
+
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+var modal2 = document.getElementById("myModal2");
+var rozmiar = document.getElementById("rozmiar");
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+var span2 = document.getElementsByClassName("close2")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function (event) {
+    if (event.target == myBtn && rozmiar.value == "" || event.target == myBtn && rozmiar.value == "off") {
+
+
+        modal.style.display = "flex";
+    } else if (event.target == myBtn && rozmiar.value == "on") {
+        modal2.style.display = "flex";
+    }
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+   
+}
+span2.onclick = function () {
+   
+    modal2.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal || event.target == modal2 ) {
+        modal.style.display = "none";
+        modal2.style.display = "none";
+    }
+}
